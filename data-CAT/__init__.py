6602f59012590893b99fb631135500d7808b1d51
Bas van Beek <b.f.van.beek@vu.nl>
2019-06-27 16:47:14 +0200

    First push

---
+++ data-CAT/__init__.py
@@ -0,0 +1,7 @@
+"""A databasing framework for the Compound Attachment Tools package (CAT)."""
+
+from .__version__ import __version__
+
+__version__ = __version__
+__author__ = 'Bas van Beek'
+__email__ = 'b.f.van.beek@vu.nl'
