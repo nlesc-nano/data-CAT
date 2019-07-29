ef5f93ba378537afa8137e16a7551ecb4445476e
Bas van Beek <b.f.van.beek@vu.nl>
2019-07-29 12:44:32 +0200

    Updated tests

---
+++ tests/test_database.py
@@ -0,0 +1,105 @@
+"""Tests for the :class:`dataCAT.database.Database` class."""
+
+from os.path import join
+import h5py
+import numpy as np
+import pandas as pd
+
+from CAT.assertion_functions import assert_exception, assert_eq, assert_id, assert_isin, Invert
+from dataCAT.database import Database
+from dataCAT.context_managers import OpenLig, OpenQD, OpenYaml
+
+PATH = join('tests', 'test_file', 'database')
+PATH = '/Users/basvanbeek/Documents/GitHub/data-CAT/tests/test_files/database'
+DB = Database(PATH)
+
+
+def test_init() -> None:
+    """Test :meth:`dataCAT.database.Database.__init__`."""
+    assert_eq(DB.path, PATH)
+
+    assert_eq(DB.csv_lig.filename, join(PATH, 'ligand_database.csv'))
+    assert_id(DB.csv_lig.manager, OpenLig)
+
+    assert_eq(DB.csv_qd.filename, join(PATH, 'QD_database.csv'))
+    assert_id(DB.csv_qd.manager, OpenQD)
+
+    assert_eq(DB.yaml.filename, join(PATH, 'job_settings.yaml'))
+    assert_id(DB.yaml.manager, OpenYaml)
+
+    assert_eq(DB.hdf5.filename, join(PATH, 'structures.hdf5'))
+    assert_id(DB.hdf5.manager, h5py.File)
+
+    assert_id(DB.mongodb, None)
+
+
+def test_str() -> None:
+    """Test :meth:`dataCAT.database.Database.__str__`."""
+    str_list = str(DB).split('\n')
+
+    assert_eq(str_list[0], 'Database(')
+    assert_eq(str_list[1], f'    dirname = {repr(PATH)},')
+    assert_eq(str_list[-2], '    mongodb = None')
+    assert_eq(str_list[-1], ')')
+
+    for item in str_list[2:-2]:
+        assert_isin('MetaManager(filename=', item)
+
+
+def test_repr() -> None:
+    """Test :meth:`dataCAT.database.Database.__repr__`."""
+    str_list = repr(DB).split('\n')
+
+    assert_eq(str_list[0], 'Database(')
+    assert_eq(str_list[1], f'    dirname = {repr(PATH)},')
+    assert_eq(str_list[-2], '    mongodb = None')
+    assert_eq(str_list[-1], ')')
+
+    for item in str_list[2:-2]:
+        assert_isin('MetaManager(filename=', item)
+
+
+def test_eq() -> None:
+    """Test :meth:`dataCAT.database.Database.__eq__`."""
+    db2 = Database(PATH)
+    assert_eq(db2, DB)
+
+
+def test_contains() -> None:
+    """Test :meth:`dataCAT.database.Database.__contains__`."""
+    assert_isin('mongodb', DB)
+    with Invert(assert_isin) as assert_not_isin:
+        assert_not_isin('bob', DB)
+
+
+def test_parse_database() -> None:
+    """Test :meth:`dataCAT.database.Database._parse_database`."""
+    out1 = DB._parse_database('ligand')
+    out2 = DB._parse_database('QD')
+
+    assert_id(out1, DB.csv_lig)
+    assert_id(out2, DB.csv_qd)
+
+    assert_exception(ValueError, DB._parse_database, 'bob')
+
+
+def test_hdf5_availability() -> None:
+    """Test :meth:`dataCAT.database.Database._parse_database`."""
+    filename = join(PATH, 'structures.hdf5')
+    with h5py.File(filename, 'r'):
+        assert_exception(OSError, DB.hdf5_availability, 1.0, 2)
+
+
+def test_from_hdf5() -> None:
+    """Test :meth:`dataCAT.database.Database.from_hdf5`."""
+    ref_tup = ('C3H7O1', 'C2H5O1', 'C1H3O1')
+
+    idx1 = slice(0, None)
+    mol_list1 = DB.from_hdf5(idx1, 'ligand', rdmol=False)
+    for mol, ref in zip(mol_list1, ref_tup):
+        assert_eq(mol.get_formula(), ref)
+
+    idx2 = np.arange(0, 2)
+    mol_list2 = DB.from_hdf5(idx2, 'ligand', rdmol=False)
+    for mol, ref in zip(mol_list2, ref_tup[0:2]):
+        assert_eq(mol.get_formula(), ref)
--- tests/test_database_functions.py
+++ tests/test_database_functions.py
@@ -1,14 +1,116 @@
 """Tests for :mod:`dataCAT.database_functions`."""
 
+from os.path import join
+
+import yaml
 import numpy as np
 import pandas as pd
 
-from CAT.assertion_functions import assert_hasattr, assert_eq, assert_id
+from rdkit import Chem
+from scm.plams import Settings
+import scm.plams.interfaces.molecule.rdkit as molkit
+
+from CAT.assertion_functions import assert_eq, assert_instance, assert_id, assert_exception
 from dataCAT.database_functions import (
     get_nan_row, as_pdb_array, from_pdb_array, sanitize_yaml_settings, even_index
 )
 
+PATH = join('tests', 'test_files')
+
 
 def test_get_nan_row() -> None:
     """Test :func:`dataCAT.database_functions.get_nan_row`."""
-    pass
+    df = pd.DataFrame(index=pd.RangeIndex(0, 10))
+    df[0] = None
+    df[1] = 0
+    df[2] = 0.0
+    df[3] = False
+    df[4] = np.datetime64('2005-02-25')
+
+    out = get_nan_row(df)
+    ref = [None, -1, np.nan, False, None]
+    assert_eq(out, ref)
+
+
+def test_as_pdb_array() -> None:
+    """Test :func:`dataCAT.database_functions.as_pdb_array`."""
+    mol_list = [molkit.readpdb(join(PATH, 'Methanol.pdb'))]
+
+    out1 = as_pdb_array(mol_list)
+    ref1 = np.array(
+        [['HETATM    1  C1  UNL     1       0.345  -0.116   0.000  1.00  0.00           C  ',
+          'HETATM    2  O1  UNL     1      -1.082  -0.219   0.000  1.00  0.00           O  ',
+          'HETATM    3  H1  UNL     1       0.733  -1.141   0.000  1.00  0.00           H  ',
+          'HETATM    4  H2  UNL     1       0.726   0.399   0.897  1.00  0.00           H  ',
+          'HETATM    5  H3  UNL     1       0.726   0.399  -0.897  1.00  0.00           H  ',
+          'HETATM    6  H4  UNL     1      -1.448   0.678   0.000  1.00  0.00           H  ',
+          'CONECT    1    2    3    4    5', 'CONECT    2    6', 'END']], dtype='|S80'
+    )
+    np.testing.assert_array_equal(out1, ref1)
+
+    out2 = as_pdb_array(mol_list, min_size=20)
+    ref2 = np.array(
+        [['HETATM    1  C1  UNL     1       0.345  -0.116   0.000  1.00  0.00           C  ',
+          'HETATM    2  O1  UNL     1      -1.082  -0.219   0.000  1.00  0.00           O  ',
+          'HETATM    3  H1  UNL     1       0.733  -1.141   0.000  1.00  0.00           H  ',
+          'HETATM    4  H2  UNL     1       0.726   0.399   0.897  1.00  0.00           H  ',
+          'HETATM    5  H3  UNL     1       0.726   0.399  -0.897  1.00  0.00           H  ',
+          'HETATM    6  H4  UNL     1      -1.448   0.678   0.000  1.00  0.00           H  ',
+          'CONECT    1    2    3    4    5', 'CONECT    2    6', 'END',
+          '', '', '', '', '', '', '', '', '', '', '']], dtype='|S80'
+    )
+    np.testing.assert_array_equal(out2, ref2)
+
+
+def test_from_pdb_array() -> None:
+    """Test :func:`dataCAT.database_functions.as_pdb_array`."""
+    mol = molkit.readpdb(join(PATH, 'Methanol.pdb'))
+
+    pdb_ar = as_pdb_array([mol])[0]
+    out1 = from_pdb_array(pdb_ar)
+    assert_instance(out1, Chem.Mol)
+
+    out2 = from_pdb_array(pdb_ar, rdmol=False)
+    for at1, at2 in zip(out2, mol):
+        assert_eq(at1.coords, at2.coords)
+        assert_eq(at1.atnum, at2.atnum)
+
+
+def test_even_index() -> None:
+    """Test :func:`dataCAT.database_functions.even_index`."""
+    df1 = pd.DataFrame(np.random.rand(10, 5))
+    df2 = pd.DataFrame(np.random.rand(20, 5))
+
+    out1 = even_index(df1, df2)
+    assert_eq(out1.shape, df2.shape)
+    np.testing.assert_array_equal(out1.index, df2.index)
+    assert np.isnan(out1.values[10:, :]).all()
+
+    out2 = even_index(df1, df1.copy())
+    assert_id(df1, out2)
+
+
+def test_sanitize_yaml_settings() -> None:
+    """Test :func:`dataCAT.database_functions.sanitize_yaml_settings`."""
+    s = Settings(yaml.load(
+        """
+        description: test
+        input:
+            ams:
+                system:
+                    bondorders:
+                        - 1 2 1.0
+                        - 1 3 1.0
+                        - 1 4 1.0
+                        - 2 6 1.0
+            uff:
+                library: uff
+
+        """, Loader=yaml.FullLoader
+    ))
+
+    out = sanitize_yaml_settings(s, 'AMSJob')
+    ref = {'input': {'uff': {'library': 'uff'}}}
+    assert_eq(out, ref)
+
+    assert_exception(KeyError, sanitize_yaml_settings, s, 'bob')
