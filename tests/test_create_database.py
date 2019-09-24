a36e8f3943ff6752b6241b29c30f17b44ab64521
Bas van Beek <b.f.van.beek@vu.nl>
2019-09-24 13:33:20 +0200

    Updated tests

--- tests/test_create_database.py
+++ tests/test_create_database.py
@@ -8,7 +8,7 @@ import h5py
 import numpy as np
 import pandas as pd
 
-from CAT.assertion_functions import (assert_eq, assert_isin, assert_exception)
+from CAT.assertion.assertion_manager import assertion
 from dataCAT.create_database import (_create_csv, _create_hdf5, _create_yaml)
 
 PATH = join('tests', 'test_files')
@@ -24,19 +24,19 @@ def test_create_csv() -> None:
     try:
         filename1 = _create_csv(PATH, 'ligand')
         filename2 = _create_csv(PATH, 'QD')
-        assert_eq(filename1, path1)
-        assert_eq(filename2, path2)
+        assertion.eq(filename1, path1)
+        assertion.eq(filename2, path2)
         df1 = pd.read_csv(path1, index_col=[0, 1], header=[0, 1], dtype=dtype1)
         df2 = pd.read_csv(path2, index_col=[0, 1, 2, 3], header=[0, 1], dtype=dtype2)
 
-        assert_eq(df1.shape, (1, 4))
-        assert_eq(df2.shape, (1, 5))
-        assert_eq(df1.index.names, ['smiles', 'anchor'])
-        assert_eq(df2.index.names, ['core', 'core anchor', 'ligand smiles', 'ligand anchor'])
-        assert_eq(df1.columns.names, ['index', 'sub index'])
-        assert_eq(df2.columns.names, ['index', 'sub index'])
-        assert_isin(('-', '-'), df1.index)
-        assert_isin(('-', '-', '-', '-'), df2.index)
+        assertion.eq(df1.shape, (1, 4))
+        assertion.eq(df2.shape, (1, 5))
+        assertion.eq(df1.index.names, ['smiles', 'anchor'])
+        assertion.eq(df2.index.names, ['core', 'core anchor', 'ligand smiles', 'ligand anchor'])
+        assertion.eq(df1.columns.names, ['index', 'sub index'])
+        assertion.eq(df2.columns.names, ['index', 'sub index'])
+        assertion.contains(('-', '-'), df1.index)
+        assertion.contains(('-', '-', '-', '-'), df2.index)
 
         np.testing.assert_array_equal(
             df1.values, np.array([[-1, 'str', False, 'str']], dtype=object)
@@ -45,7 +45,7 @@ def test_create_csv() -> None:
             df2.values, np.array([[-1, -1, False, 'str', 'str']], dtype=object)
         )
 
-        assert_exception(ValueError, _create_csv, PATH, 'bob')
+        assertion.exception(ValueError, _create_csv, PATH, 'bob')
     finally:
         remove(path1)
         remove(path2)
@@ -59,14 +59,14 @@ def test_create_hdf5() -> None:
 
     try:
         filename = _create_hdf5(PATH)
-        assert_eq(filename, path)
+        assertion.eq(filename, path)
         with h5py.File(path, 'r') as f:
             for item in ref_keys1:
-                assert_isin(item, f.keys())
-                assert_eq(f[item].ndim, 2)
+                assertion.contains(item, f.keys())
+                assertion.eq(f[item].ndim, 2)
             for item in ref_keys2:
-                assert_isin(item, f.keys())
-                assert_eq(f[item].ndim, 3)
+                assertion.contains(item, f.keys())
+                assertion.eq(f[item].ndim, 3)
     finally:
         remove(path)
 
@@ -77,9 +77,9 @@ def test_create_yaml() -> None:
 
     try:
         filename = _create_yaml(PATH)
-        assert_eq(filename, path)
+        assertion.eq(filename, path)
         with open(path, 'r') as f:
             out = yaml.load(f, Loader=yaml.FullLoader)
-        assert_eq(out, {None: [None]})
+        assertion.eq(out, {None: [None]})
     finally:
         remove(path)
--- tests/test_database.py
+++ tests/test_database.py
@@ -5,7 +5,7 @@ import h5py
 import numpy as np
 import pandas as pd
 
-from CAT.assertion_functions import assert_exception, assert_eq, assert_id, assert_isin, Invert
+from CAT.assertion.assertion_manager import assertion
 from dataCAT.database import Database
 from dataCAT.context_managers import OpenLig, OpenQD, OpenYaml
 
@@ -19,60 +19,59 @@ OPT = ('opt', '')
 
 def test_init() -> None:
     """Test :meth:`dataCAT.database.Database.__init__`."""
-    assert_eq(DB.dirname, PATH)
+    assertion.eq(DB.dirname, PATH)
 
-    assert_eq(DB.csv_lig.filename, join(PATH, 'ligand_database.csv'))
-    assert_id(DB.csv_lig.manager, OpenLig)
+    assertion.eq(DB.csv_lig.filename, join(PATH, 'ligand_database.csv'))
+    assertion.is_(DB.csv_lig.manager, OpenLig)
 
-    assert_eq(DB.csv_qd.filename, join(PATH, 'QD_database.csv'))
-    assert_id(DB.csv_qd.manager, OpenQD)
+    assertion.eq(DB.csv_qd.filename, join(PATH, 'QD_database.csv'))
+    assertion.is_(DB.csv_qd.manager, OpenQD)
 
-    assert_eq(DB.yaml.filename, join(PATH, 'job_settings.yaml'))
-    assert_id(DB.yaml.manager, OpenYaml)
+    assertion.eq(DB.yaml.filename, join(PATH, 'job_settings.yaml'))
+    assertion.is_(DB.yaml.manager, OpenYaml)
 
-    assert_eq(DB.hdf5.filename, join(PATH, 'structures.hdf5'))
-    assert_id(DB.hdf5.manager, h5py.File)
+    assertion.eq(DB.hdf5.filename, join(PATH, 'structures.hdf5'))
+    assertion.is_(DB.hdf5.manager, h5py.File)
 
-    assert_id(DB.mongodb, None)
+    assertion.is_(DB.mongodb, None)
 
 
 def test_str() -> None:
     """Test :meth:`dataCAT.database.Database.__str__`."""
     str_list = str(DB).split('\n')
 
-    assert_eq(str_list[0], 'Database(')
-    assert_eq(str_list[1], f'    dirname = {repr(PATH)},')
-    assert_eq(str_list[-2], '    mongodb = None')
-    assert_eq(str_list[-1], ')')
+    assertion.eq(str_list[0], 'Database(')
+    assertion.eq(str_list[1], f'    dirname = {repr(PATH)},')
+    assertion.eq(str_list[-2], '    mongodb = None')
+    assertion.eq(str_list[-1], ')')
 
     for item in str_list[2:-2]:
-        assert_isin('MetaManager(filename=', item)
+        assertion.contains('MetaManager(filename=', item)
 
 
 def test_repr() -> None:
     """Test :meth:`dataCAT.database.Database.__repr__`."""
     str_list = repr(DB).split('\n')
 
-    assert_eq(str_list[0], 'Database(')
-    assert_eq(str_list[1], f'    dirname = {repr(PATH)},')
-    assert_eq(str_list[-2], '    mongodb = None')
-    assert_eq(str_list[-1], ')')
+    assertion.eq(str_list[0], 'Database(')
+    assertion.eq(str_list[1], f'    dirname = {repr(PATH)},')
+    assertion.eq(str_list[-2], '    mongodb = None')
+    assertion.eq(str_list[-1], ')')
 
     for item in str_list[2:-2]:
-        assert_isin('MetaManager(filename=', item)
+        assertion.contains('MetaManager(filename=', item)
 
 
 def test_eq() -> None:
     """Test :meth:`dataCAT.database.Database.__eq__`."""
     db2 = Database(PATH)
-    assert_eq(db2, DB)
+    assertion.eq(db2, DB)
 
 
 def test_contains() -> None:
     """Test :meth:`dataCAT.database.Database.__contains__`."""
-    assert_isin('mongodb', DB)
-    with Invert(assert_isin) as assert_not_isin:
-        assert_not_isin('bob', DB)
+    assertion.contains('mongodb', DB)
+    assertion.contains('bob', DB, invert=True)
 
 
 def test_parse_database() -> None:
@@ -80,17 +79,17 @@ def test_parse_database() -> None:
     out1 = DB._parse_database('ligand')
     out2 = DB._parse_database('QD')
 
-    assert_id(out1, DB.csv_lig)
-    assert_id(out2, DB.csv_qd)
+    assertion.is_(out1, DB.csv_lig)
+    assertion.is_(out2, DB.csv_qd)
 
-    assert_exception(ValueError, DB._parse_database, 'bob')
+    assertion.exception(ValueError, DB._parse_database, 'bob')
 
 
 def test_hdf5_availability() -> None:
     """Test :meth:`dataCAT.database.Database._parse_database`."""
     filename = join(PATH, 'structures.hdf5')
     with h5py.File(filename, 'r'):
-        assert_exception(OSError, DB.hdf5_availability, 1.0, 2)
+        assertion.exception(OSError, DB.hdf5_availability, 1.0, 2)
 
 
 def test_from_hdf5() -> None:
@@ -100,12 +99,12 @@ def test_from_hdf5() -> None:
     idx1 = slice(0, None)
     mol_list1 = DB.from_hdf5(idx1, 'ligand', rdmol=False)
     for mol, ref in zip(mol_list1, ref_tup):
-        assert_eq(mol.get_formula(), ref)
+        assertion.eq(mol.get_formula(), ref)
 
     idx2 = np.arange(0, 2)
     mol_list2 = DB.from_hdf5(idx2, 'ligand', rdmol=False)
     for mol, ref in zip(mol_list2, ref_tup[0:2]):
-        assert_eq(mol.get_formula(), ref)
+        assertion.eq(mol.get_formula(), ref)
 
 
 def test_from_csv() -> None:
@@ -123,9 +122,9 @@ def test_from_csv() -> None:
     df[OPT] = True
 
     out1 = DB.from_csv(df, 'ligand', get_mol=False)
-    assert_id(out1, None)
+    assertion.is_(out1, None)
 
     ref_tup = ('C3H7O1', 'C2H5O1', 'C1H3O1')
     out2 = DB.from_csv(df, 'ligand', get_mol=True, inplace=False)
     for mol, ref in zip(out2, ref_tup):
-        assert_eq(mol.get_formula(), ref)
+        assertion.eq(mol.get_formula(), ref)
