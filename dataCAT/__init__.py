07dd40421ca0938d0511c4c7f66667f6f947f246
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-16 15:49:28 +0200

    WIP: Overhaul of the .pdb storage system

--- dataCAT/__init__.py
+++ dataCAT/__init__.py
@@ -9,6 +9,7 @@ from .__version__ import __version__
 version_info = VersionInfo.from_str(__version__)
 del VersionInfo
 
+from .pdb_array import DTYPE_ATOM, DTYPE_BOND, PDBTuple
 from .df_proxy import DFProxy
 from .context_managers import OpenYaml, OpenLig, OpenQD
 from .database_functions import df_to_mongo_dict
@@ -18,6 +19,7 @@ __author__ = 'B. F. van Beek'
 __email__ = 'b.f.van.beek@vu.nl'
 
 __all__ = [
+    'DTYPE_ATOM', 'DTYPE_BOND', 'PDBTuple',
     'DFProxy',
     'OpenYaml', 'OpenLig', 'OpenQD',
     'df_to_mongo_dict',
---
+++ dataCAT/pdb_array.py
@@ -0,0 +1,455 @@
+"""A module for constructing array-representations of .pdb files.
+
+Index
+-----
+.. currentmodule:: dataCAT
+.. autosummary::
+    PDBTuple
+    PDBTuple.from_molecules
+    PDBTuple.to_molecules
+    DTYPE_ATOM
+    DTYPE_BOND
+
+API
+---
+.. autoclass:: PDBTuple
+.. automethod:: PDBTuple.from_molecules
+.. automethod:: PDBTuple.to_molecules
+.. data:: DTYPE_ATOM
+    :value: pandas.DataFrame(...)
+
+    A :class:`pandas.DataFrame` representing the dtype of :attr:`PDBTuple.atoms`.
+
+    Most field names are based on to their, identically named, counterpart as produced by
+    :func:`readpdb()<scm.plams.interfaces.molecule.rdkit.readpdb>`,
+    the data in question being stored in the
+    :class:`Atom.properties.pdb_info<scm.plams.mol.atom.Atom>` block.
+
+    There are four exception to this general rule:
+
+    * ``coords``: Based on :class:`Atom.coords<scm.plams.mol.atom.Atom>`.
+    * ``symbol``: Based on :class:`Atom.symbol<scm.plams.mol.atom.Atom>`.
+    * ``charge``: Based on :class:`Atom.properties.charge<scm.plams.mol.atom.Atom>`.
+    * ``charge_float``: Based on :class:`Atom.properties.charge_float<scm.plams.mol.atom.Atom>`.
+
+    .. code:: python
+
+        >>> from dataCAT import DTYPE_ATOM
+        >>> print(DTYPE_ATOM)  # doctest: +SKIP
+{dtype_atom}
+
+
+.. data:: DTYPE_BOND
+    :value: pandas.DataFrame(...)
+
+    A :class:`pandas.DataFrame` representing the dtype of :attr:`PDBTuple.bonds`.
+
+    .. code:: python
+
+        >>> from dataCAT import DTYPE_BOND
+        >>> print(DTYPE_BOND)
+{dtype_bond}
+
+"""
+
+import textwrap
+from collections import abc
+from itertools import repeat
+from typing import List, Tuple, Collection, NamedTuple, Iterable, Union, Type, TypeVar, Optional, overload, Generator, Sequence
+
+import numpy as np
+import pandas as pd
+from scm.plams import Molecule, Atom, Bond
+from nanoutils import TypedDict, SupportsIndex
+
+__all__ = ['DTYPE_ATOM', 'DTYPE_BOND', 'PDBTuple']
+
+ST = TypeVar('ST', bound='PDBTuple')
+
+_DType = List[Tuple[
+    str,   # Name
+    np.dtype,  # Data type
+    Union[int, Tuple[int, ...]]  # Shape
+]]
+
+DTYPE_ATOM = pd.Series({
+    'IsHeteroAtom': bool,
+    'SerialNumber': 'int16',
+    'Name': 'S4',
+    'ResidueName': 'S3',
+    'ChainId': 'S1',
+    'ResidueNumber': 'int16',
+    'coords': float,
+    'Occupancy': float,
+    'TempFactor': float,
+    'symbol': 'S4',
+    'charge': 'int8',
+    'charge_float': float
+}, name='dtype', dtype=object).to_frame()
+DTYPE_ATOM.index.name = 'name'
+DTYPE_ATOM['dtype'] = [np.dtype(i) for i in DTYPE_ATOM['dtype']]
+DTYPE_ATOM['shape'] = 0
+DTYPE_ATOM.at['coords', 'shape'] = 3
+
+_DTYPE_ATOM: _DType = [
+    (i, dtype, shape or ()) for i, (dtype, shape) in DTYPE_ATOM.iterrows()
+]
+
+DTYPE_BOND = pd.Series({
+    'atoms': int,
+    'order': 'int8'
+}, name='dtype', dtype=object).to_frame()
+DTYPE_BOND.index.name = 'name'
+DTYPE_BOND['dtype'] = [np.dtype(i) for i in DTYPE_BOND['dtype']]
+DTYPE_BOND['shape'] = 0
+DTYPE_BOND.at['atoms', 'shape'] = 2
+
+_DTYPE_BOND: _DType = [
+    (i, dtype, shape or ()) for i, (dtype, shape) in DTYPE_BOND.iterrows()
+]
+
+_AtomTuple = Tuple[
+    bool,  # IsHeteroAtom
+    int,  # SerialNumber
+    str,  # Name
+    str,  # ResidueName
+    str,  # ChainId
+    int,  # ResidueNumber
+    Tuple[float, float, float],  # coords
+    float,  # Occupancy
+    float,  # TempFactor
+    str,  # symbol
+    int,  # charge
+    float  # charge_float
+]
+
+_BondTuple = Tuple[
+    Tuple[int, int],  # atom1, atom2
+    int,  # order
+]
+
+
+def _get_atom_info(at: Atom, i: int) -> _AtomTuple:
+    prop = at.properties
+    symbol = at.symbol
+    charge = prop.get('charge', 0)
+
+    pdb = prop.get('pdb_info', {})
+
+    return (
+        pdb.get('IsHeteroAtom', False),
+        pdb.get('SerialNumber', i),
+        pdb.get('Name', symbol),
+        pdb.get('ResidueName', 'LIG'),
+        pdb.get('ChainId', 'A'),
+        pdb.get('ResidueNumber', 1),
+        at.coords,
+        pdb.get('Occupancy', 1.0),
+        pdb.get('TempFactor', 0.0),
+        symbol,
+        charge,
+        prop.get('charge_float', charge)
+    )
+
+
+def _get_bond_info(mol: Molecule) -> List[_BondTuple]:
+    mol.set_atoms_id(start=1)
+    ret = [((b.atom1.id, b.atom2.id), b.order) for b in mol.bonds]
+    mol.unset_atoms_id()
+    return ret
+
+
+class _PDBInfo(TypedDict):
+    IsHeteroAtom: bool
+    SerialNumber: int
+    Name: str
+    ResidueName: str
+    ChainId: str
+    ResidueNumber: int
+    Occupancy: float
+    TempFactor: float
+
+
+class _Properties(TypedDict):
+    charge: int
+    charge_float: float
+    pdb_info: _PDBInfo
+
+
+_3Tup = Tuple[_Properties, Tuple[float, float, float], str]
+
+
+def _iter_rec(atom_array: np.recarray) -> Generator[_3Tup, None, None]:
+    for IsHeteroAtom, SerialNumber, Name, ResidueName, ChainId, ResidueNumber, coords, Occupancy, TempFactor, symbol, charge, charge_float in atom_array:  # noqa: E501
+        _pdb_info: _PDBInfo = {
+            'IsHeteroAtom': IsHeteroAtom,
+            'SerialNumber': SerialNumber,
+            'Name': Name.decode(),
+            'ResidueName': ResidueName.decode(),
+            'ChainId': ChainId.decode(),
+            'ResidueNumber': ResidueNumber,
+            'Occupancy': Occupancy,
+            'TempFactor': TempFactor
+        }
+
+        properties: _Properties = {
+            'charge': charge,
+            'charge_float': charge_float,
+            'pdb_info': _pdb_info
+        }
+        _coords: Tuple[float, float, float] = tuple(coords)  # type: ignore
+        yield properties, _coords, symbol.decode()
+
+
+def rec_to_mol(atom_array: np.recarray, bond_array: np.recarray,
+               atom_len: Optional[int] = None,
+               bond_len: Optional[int] = None,
+               mol: Optional[Molecule] = None) -> Molecule:
+    """Construct a molecule from the passed **atom_array** and **bond_array**.
+    """
+    if mol is None:
+        ret = Molecule()
+        for _ in range(len(atom_array[:atom_len])):
+            ret.add_atom(Atom(mol=ret))
+    else:
+        ret = mol
+
+    iterator = _iter_rec(atom_array[:atom_len])
+    for atom, (properties, coords, symbol) in zip(ret, iterator):
+        atom.coords = coords
+        atom.symbol = symbol
+        atom.properties.update(properties)
+
+    if ret.bonds:
+        ret.delete_all_bonds()
+    for (i, j), order in bond_array[:bond_len]:
+        bond = Bond(atom1=ret[i], atom2=ret[j], order=order, mol=ret)
+        ret.add_bond(bond)
+    return ret
+
+
+class PDBTuple(NamedTuple):
+    """A :func:`~collections.namedtuple` for holding an array-like represention of a set of .pdb files.
+
+    Examples
+    --------
+    .. code:: python
+
+        >>> from dataCAT import PDBTuple
+
+        >>> pdb_tup = PDBTuple(...)  # doctest: +SKIP
+        >>> print(pdb_tup)  # doctest: +SKIP
+        PDBTuple(
+            atoms      = numpy.recarray(..., shape=(23, 2099), dtype=...),
+            bonds      = numpy.recarray(..., shape=(23, 1950), dtype=...),
+            atom_count = numpy.ndarray(..., shape=(23,), dtype=int64),
+            bond_count = numpy.ndarray(..., shape=(23,), dtype=int64)
+        )
+
+    Attributes
+    ----------
+    atoms : :class:`numpy.recarray`, shape :math:`(n, m)`
+        A padded recarray for keeping track of all atom-related information.
+        See :data:`dataCAT.DTYPE_ATOM` for a comprehensive overview of
+        all field names, dtypes and shapes.
+    bonds : :class:`numpy.recarray`, shape :math:`(n, k)`
+        A padded recarray for keeping track of all bond-related information.
+        See :data:`dataCAT.DTYPE_BOND` for a comprehensive overview of
+        all field names, dtypes and shapes.
+    atom_count : :class:`numpy.ndarray[int]<numpy.ndarray>`, shape :math:`(n,)`
+        An ndarray for keeping track of the number of atoms in each molecule in :attr:`atoms`.
+    bond_count : :class:`numpy.ndarray[int]<numpy.ndarray>`, shape :math:`(n,)`
+        An ndarray for keeping track of the number of bonds in each molecule in :attr:`bonds`.
+
+    """  # noqa: E501
+
+    atoms: np.recarray
+    bonds: np.recarray
+    atom_count: np.ndarray
+    bond_count: np.ndarray
+
+    def __repr__(self) -> str:
+        """Implement :class:`str(self)<str>` and :func:`repr(self)<repr>`."""
+        try:
+            wdith = max(len(k) for k in self._fields)
+        except ValueError:
+            return f'{self.__class__.__name__}()'
+
+        def _str(k, v):
+            if isinstance(v, np.recarray):
+                dtype = '...'
+            else:
+                dtype = str(v.dtype)
+            return (f'{k:{wdith}} = {v.__class__.__module__}.{v.__class__.__name__}'
+                    f'(..., shape={v.shape}, dtype={dtype})')
+
+        ret = ',\n'.join(_str(k, v) for k, v in zip(self._fields, self))
+        indent = 4 * ' '
+        return f'{self.__class__.__name__}(\n{textwrap.indent(ret, indent)}\n)'
+
+    @classmethod
+    def from_molecules(cls: Type[ST], mol_list: Iterable[Molecule],
+                       min_atom: int = 0,
+                       min_bond: int = 0) -> ST:
+        """Convert an iterable or sequence of molecules into a new :class:`PDBTuple` instance.
+
+        Examples
+        --------
+        .. code:: python
+
+            >>> from typing import List
+            >>> from dataCAT import PDBTup
+            >>> from scm.plams import readpdb, Molecule
+
+            >>> mol_list: List[Molecule] = [readpdb(...), ...]  # doctest: +SKIP
+            >>> PDBTup.from_molecules(mol_list)
+            PDBTuple(
+                atoms      = numpy.recarray(..., shape=(23, 2099), dtype=...),
+                bonds      = numpy.recarray(..., shape=(23, 1950), dtype=...),
+                atom_count = numpy.ndarray(..., shape=(23,), dtype=int64),
+                bond_count = numpy.ndarray(..., shape=(23,), dtype=int64)
+            )
+
+        Parameters
+        ----------
+        mol_list : :class:`Iterable[Molecule]<typing.Iterable>`
+            An iterable consisting of PLAMS molecules.
+        min_atom : :class:`int`
+            The minimum number of atoms which :attr:`PDBTuple.atoms` should accomodate.
+        min_bond : :class:`int`
+            The minimum number of atoms which :attr:`PDBTuple.bonds` should accomodate.
+
+        Returns
+        -------
+        :class:`PDBTuple`
+            A new namedtuple.
+
+        """
+        if isinstance(mol_list, abc.Iterator):
+            mol_list_: Collection[Molecule] = list(mol_list)
+        else:
+            mol_list_ = mol_list  # type: ignore
+        mol_count = len(mol_list_)
+
+        # Gather the shape of the to-be created atom (pdb-file) array
+        _atom_count = max(len(mol.atoms) for mol in mol_list_)
+        atom_count = max(_atom_count, min_atom)
+        atom_shape = mol_count, atom_count
+
+        # Gather the shape of the to-be created bond array
+        _bond_count = max(len(mol.bonds) for mol in mol_list_)
+        bond_count = max(_bond_count, min_bond)
+        bond_shape = mol_count, bond_count
+
+        # Construct the to-be returned (padded) arrays
+        atom_array = np.rec.array(None, shape=atom_shape, dtype=_DTYPE_ATOM)
+        bond_array = np.rec.array(None, shape=bond_shape, dtype=_DTYPE_BOND)
+        atom_counter = np.empty(mol_count, dtype=int)
+        bond_counter = np.empty(mol_count, dtype=int)
+
+        # Fill the to-be returned arrays
+        for i, mol in enumerate(mol_list_):
+            j_atom = len(mol.atoms)
+            j_bond = len(mol.bonds)
+
+            atom_array[i, :j_atom] = [_get_atom_info(at, k) for k, at in enumerate(mol, 1)]
+            bond_array[i, :j_bond] = _get_bond_info(mol)
+            atom_counter[i] = j_atom
+            bond_counter[i] = j_bond
+
+        return cls(atoms=atom_array, atom_count=atom_counter,
+                   bonds=bond_array, bond_count=bond_counter)
+
+    @overload
+    def to_molecules(self, idx: Union[None, Sequence[int], slice, np.ndarray] = ..., mol: Optional[Iterable[Optional[Molecule]]] = ...) -> List[Molecule]:
+        ...
+    @overload
+    def to_molecules(self, idx: SupportsIndex = ..., mol: Optional[Molecule] = ...) -> Molecule:
+        ...
+    def to_molecules(self, idx=None, mol=None):
+        """Create a molecule or list of molecules from this instance.
+
+        Examples
+        --------
+        .. code:: python
+
+            >>> from dataCAT import PDBTup
+            >>> from scm.plams import Molecule
+
+            >>> pdb_tup = PDBTup(...)
+
+            # Create a single new molecule from `pdb_tup`
+            >>> pdb_tup.as_molecules(idx=0)
+            <scm.plams.mol.molecule.Molecule at ...>
+
+            # Create three new molecules from `pdb_tup`
+            >>> pdb_tup.as_molecules(idx=[0, 1])
+            [<scm.plams.mol.molecule.Molecule at ...>,
+             <scm.plams.mol.molecule.Molecule at ...>]
+
+            # Update `mol` with the info from `pdb_tup`
+            >>> mol1 = Molecule(...)
+            >>> mol2 = pdb_tup.as_molecules(idx=2, mol=mol1)
+            >>> mol1 is mol2
+            True
+
+            # Update all molecules in `mol_list` with info from `pdb_tup`
+            >>> mol_list1 = [Molecule(...), Molecule(...), Molecule(...)]
+            >>> mol_list2 = pdb_tup.as_molecules(idx=range(3), mol=mol_list)
+            >>> all((m1 is m2) for m1, m2 in zip(mol_list1, mol_list2))
+            True
+
+        Parameters
+        ----------
+        idx : :class:`int` or :class:`Sequence[int]<typing.Sequence>`, optional
+            An object for slicing the arrays embedded within this instance.
+            Follows the standard numpy broadcasting rules (*e.g.* :code:`self.atoms[idx]`).
+            If a scalar is provided (*e.g.* an integer) then a single molecule will be returned.
+            If a sequence, range, slice, *etc.* is provided then
+            a list of molecules will be returned.
+        mol : :class:`~scm.plams.mol.molecule.Molecule`, :class:`Iterable[Molecule]<typing.Iterable>`, optional
+            A molecule or list of molecules.
+            If one or molecules are provided here then they will be updated inplace.
+
+        Returns
+        -------
+        :class:`~scm.plams.mol.molecule.Molecule` or :class:`List[Molecule]<typing.Molecule>`
+            A molecule or list of molecules,
+            depending on whether or not **idx** is a scalar or sequence / slice.
+            Note that if :code:`mol is not None`, then the-be returned molecules won't be copies.
+
+        """  # noqa: E501
+        if idx is None:
+            i = slice(None)
+            is_seq = True
+        else:
+            try:
+                i = idx.__index__()
+                is_seq = False
+            except (AttributeError, TypeError):
+                i = idx
+                is_seq = True
+
+        atoms = self.atoms[i]
+        bonds = self.bonds[i]
+        atom_count = self.atom_count[i]
+        bond_count = self.bond_count[i]
+
+        if not is_seq:
+            return rec_to_mol(atoms, bonds, atom_count, bond_count, mol)
+
+        if mol is None:
+            mol_list = repeat(None)
+        elif isinstance(mol, Molecule):
+            raise TypeError
+        else:
+            mol_list = mol
+
+        iterator = zip(atoms, bonds, atom_count, bond_count, mol_list)
+        return [rec_to_mol(*args) for args in iterator]
+
+
+__doc__ = __doc__.format(
+    dtype_atom=textwrap.indent(repr(DTYPE_ATOM), 8 * ' '),
+    dtype_bond=textwrap.indent(repr(DTYPE_BOND), 8 * ' ')
+)
