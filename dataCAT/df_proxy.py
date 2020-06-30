0e58169d33f6c3f9517a87b1e2e562dc6b38dcce
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-30 12:34:45 +0200

    Be more accurate about the __module__

--- dataCAT/df_proxy.py
+++ dataCAT/df_proxy.py
@@ -44,7 +44,7 @@ class _DFMeta(type):
     NDTYPE: Type[NDFrame] = NotImplemented
 
     @staticmethod
-    def _construct_getter(cls_name: str, func_name: str) -> property:
+    def _construct_getter(cls_name: str, func_name: str, module: str = __name__) -> property:
         """Helper function for :meth:`_DFMeta.__new__`; constructs a :class:`property` getter."""
         def getter(self):
             return getattr(self.ndframe, func_name)
@@ -52,11 +52,12 @@ class _DFMeta(type):
         getter.__doc__ = f"""Get :meth:`pandas.DataFrame.{func_name}` of :attr:`{cls_name}.ndframe`."""  # noqa: E501
         getter.__name__ = func_name
         getter.__qualname__ = f'{cls_name}.{func_name}'
-        getter.__module__ = __name__
+        getter.__module__ = module
         return property(getter)
 
     @staticmethod
-    def _construct_setter(prop: property, cls_name: str, func_name: str) -> property:
+    def _construct_setter(prop: property, cls_name: str, func_name: str,
+                          module: str = __name__) -> property:
         """Helper function for :meth:`_DFMeta.__new__`; constructs a :class:`property` setter."""
         def setter(self, value):
             return setattr(self.ndframe, func_name, value)
@@ -64,13 +65,14 @@ class _DFMeta(type):
         setter.__doc__ = f"""Set :meth:`pandas.DataFrame.{func_name}` of :attr:`{cls_name}.ndframe`."""  # noqa: E501
         setter.__name__ = func_name
         setter.__qualname__ = f'{cls_name}.{func_name}'
-        setter.__module__ = __name__
+        setter.__module__ = module
         return prop.setter(setter)
 
     def __new__(mcls: Type[TT], name: str, bases: Tuple[type, ...],  # noqa: N804
                 namespace: Dict[str, Any]) -> TT:
         """Construct a new :class:`_DFMeta` instance."""
         cls = cast(TT, super().__new__(mcls, name, bases, namespace))
+        module = cls.__module__
 
         # Validation
         try:
@@ -80,15 +82,15 @@ class _DFMeta(type):
             raise TypeError(f"{name}.NDTYPE expectes an NDFrame subclass") from ex
 
         # Construct properties linking to attributes of **cls**
-        name_iterator = (i for i in dir(cls.NDTYPE) if not i.startswith('_'))
+        name_iterator = (i for i in dir(ndtype) if not i.startswith('_'))
         for func_name in chain(mcls.MAGIC, name_iterator):
-            getter = mcls._construct_getter(name, func_name)
+            getter = mcls._construct_getter(name, func_name, module=module)
             setattr(cls, func_name, getter)
 
         # Construct setters
         for func_name in mcls.SETTERS:
             getter = getattr(cls, func_name)
-            setter = mcls._construct_setter(getter, name, func_name)
+            setter = mcls._construct_setter(getter, name, func_name, module=module)
             setattr(cls, func_name, setter)
         return cls
 
