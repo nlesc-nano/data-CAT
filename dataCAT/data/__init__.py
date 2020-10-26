2231df525c520c6ba5b97b2d1b128f7162bf0da8
Bas van Beek <b.f.van.beek@vu.nl>
2020-10-26 17:09:09 +0100

    MAINT: Sort the tuple of .pdb files

--- dataCAT/data/__init__.py
+++ dataCAT/data/__init__.py
@@ -19,9 +19,9 @@ from typing import Tuple
 _DATA = os.path.abspath(os.path.dirname(__file__))
 
 #: A tuple with the absolute paths to all .pdb files in :mod:`dataCAT.data`.
-PDB_TUPLE: Tuple[str, ...] = tuple(
+PDB_TUPLE: Tuple[str, ...] = tuple(sorted(
     os.path.join(_DATA, f) for f in os.listdir(_DATA) if f.endswith('pdb')
-)
+))
 del _DATA
 del os
 
